56c6e789dc2e16aaba51b05bd67f28266e041387
Gabriel Lira <gabriel.lira@autoglass.com.br>
2022-07-29 14:55:09 -0300

    Adicionando arquivos necessários para desenvolver o projeto

---
+++ ByteBank7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ByteBank7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(876, 86712540);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.WriteLine("Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
